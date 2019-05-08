0243449b6ffe917ff5dc9af5b364e028fe44f5d8
Ashish Garg <mragarg@gmail.com>
2019-05-08 11:31:52 -0400

    Using initialState and create new Objects in the reducer function

--- src/index.js
+++ src/index.js
@@ -41,13 +41,28 @@
 
 import { createStore } from 'redux';
 
-const reducer = (state, action) => {
+const initialState = {
+    result: 1,
+    lastValues: [],
+    username: "Max"
+}
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
             break;
         case "SUBTRACT":
-            // Logic
+            // state.result -= action.payload;      // DON'T DO THIS BECAUSE STATE IS IMMUTABLE
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            }
             break;
     }
 
@@ -55,8 +70,8 @@ const reducer = (state, action) => {
     return state;
 };
 
-// Second arugment is the inital state
-const store = createStore(reducer, 1);
+// Second arugment is the inital state (removed because created initialState Object)
+const store = createStore(reducer);
 
 // Called whenever store is update
 store.subscribe(() => {
@@ -66,5 +81,15 @@ store.subscribe(() => {
 // Dispatch expects a JS Object
 store.dispatch({
     type: "ADD",
-    payload: 10   // Payload is the value of the action
-});
+    payload: 100   // Payload is the value of the action
+}); // Expect 101
+
+store.dispatch({
+    type: "ADD",
+    payload: 22
+}); // Expect 123
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 80
+}); // Expect 43
