8c903e98f3dd59c15fef02664014a7fb1a3afb31
Ashish Garg <mragarg@gmail.com>
2019-05-08 10:33:06 -0400

    Install react-redux and redux. Skeleton for redux in Index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import * as serviceWorker from './serviceWorker';
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+// import { User } from './components/User';
+// import { Main } from './components/Main';
 
-class App extends React.Component {
-    constructor(props) {
-      super(props);
+// class App extends React.Component {
+//     constructor(props) {
+//       super(props);
     
-      this.state = {
-         username: "Max"
-      }
-    }
+//       this.state = {
+//          username: "Max"
+//       }
+//     }
 
-    _changeUsername = (newName) => {
-        this.setState({
-            username: newName
-        });
-    }
+//     _changeUsername = (newName) => {
+//         this.setState({
+//             username: newName
+//         });
+//     }
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this._changeUsername} />
-                <User username={this.state.username} />
-            </div>
-        );
-    }
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this._changeUsername} />
+//                 <User username={this.state.username} />
+//             </div>
+//         );
+//     }
     
-}
+// }
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            // Logic
+            break;
+        case "SUBTRACT":
+            // Logic
+            break;
+    }
+
+    // Always return a state because that's the state the App will use
+    return state;
+};
+
+const store = createStore(reducer, 1);
